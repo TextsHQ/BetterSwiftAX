ce3c589049c1d1065a60c011b564717a003acb79
Max <8418866+rdev@users.noreply.github.com>
2022-01-20 00:31:42 +0800

    onboarding: show overlay for accessibility access (#28)
    Co-authored-by: Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftServer",
-    platforms: [.macOS(.v10_11)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "SwiftServer",
