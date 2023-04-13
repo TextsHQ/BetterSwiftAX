f238ea9e89868bcaa3e1ca2256cc19f0b29cb3cb
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-04-12 21:06:41 -0700

    SwiftServer: drop mojave support
    known issue in xcode 14.3
    
    https://github.com/apple/swift/pull/64412
    
    cannot fix without compiler changes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftServer",
-    platforms: [.macOS(.v10_14)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "SwiftServer",
